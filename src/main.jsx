7e5061edd8ef545987e354a5443088ffb9641496
emami-linux <emami.114@outlook.de>
2025-06-05 17:36:22 +0200

    Update Hash

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter as Router} from 'react-router-dom'; // Neue Version
+
 import './index.css'
-import App from './App.jsx'
 import Quiz from './Quiz.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Router>
   <Quiz />
-  </StrictMode>,
+  </Router>,
 )
